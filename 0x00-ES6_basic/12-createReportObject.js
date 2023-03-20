f45483cb62ee7cffcbc7d5aa9cf576040642f090
Thabiso <940303tabiso@gmail.com>
2023-03-20 13:08:21 +0000

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,20 +1,3 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-  
-  employeesList.forEach((employee) => {
-    const { department, name } = employee;
-    
-    if (!allEmployees[department]) {
-      allEmployees[department] = [];
-    }
-    
-    allEmployees[department] = [...allEmployees[department], name];
-  });
-  
-  return {
-    allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(allEmployees).length;
-    },
-  };
+  return { allEmployees: { ...employeesList } };
 }
