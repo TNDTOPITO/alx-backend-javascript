5b4f514513401150ff768021fd23e40ced6eea85
Thabiso <940303tabiso@gmail.com>
2023-03-27 20:15:29 +0000

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (startString && typeof startString === 'string') {
     const strings = [];
     for (const item of set) {
-      if (item.startsWith(startString)) {
+      if (item.toLowerCase().startsWith(startString.toLowerCase())) {
         strings.push(item.slice(startString.length));
       }
     }
